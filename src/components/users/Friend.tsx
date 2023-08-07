03dbc3e77e19c07c17034dffebb3b57dbd5cb577
alyssa-tn <tn.alyssa@gmail.com>
2023-08-06 22:42:20 -0500

    small css edit

--- src/components/users/Friend.tsx
+++ src/components/users/Friend.tsx
@@ -1,21 +1,16 @@
+import './Friend.css'
+
 type FriendProps = { 
     id: string 
     profilePic: string
 }
 
 export const Friend = ({profilePic}: FriendProps)  => {
-    // return (
-    //     <div className="friend">
-    //         {props.id}
-    //         {props.profilePic}
-    //     </div>
-    // )
     return (
         <div className="friend">
-            {/* {profilePic === null 
-            ? <img src={'https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg'} alt="friend" className="default"/> 
-            : <img src={profilePic} alt="friend"/>} */}
-            <img src={profilePic === null ? 'https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg': profilePic} />
+            <img className="friend-card" src={profilePic === null 
+                ? 'https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg'
+                : profilePic} />
         </div>
     )
 }
