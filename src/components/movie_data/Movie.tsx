4c829de389e5e1eb21d769e72d365afea7bf6c9e
Whitney <whitneyshake@gmail.com>
2023-08-09 15:27:36 -0700

    Three dot menu ready for connetion

--- src/components/movie_data/Movie.tsx
+++ src/components/movie_data/Movie.tsx
@@ -5,8 +5,21 @@ import './Movie.css'
 export const Movie = ({posterPath}: MovieProps) => {
     return (
         <div className="card-container">
-            <div>
-                <img className="card" src={posterPath === null 
+            <div className="movie-container">
+                {/* <button className="dot-button">...</button> */}
+                <div className="dropdown">
+                    <div className="dropdown-button">
+                        <span className="dot"></span>
+                        <span className="dot"></span>
+                        <span className="dot"></span>
+                    </div>
+                    <div className="dropdown-content">
+                        <a href="#">View More Details</a>
+                        <a href="#">Mark As Watched</a>
+                        <a href="#">Add Movie to Playlist</a>
+                    </div>
+                </div>
+                <img className="card" alt="movie cover" src={posterPath === null 
                 ? 'https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg'
                 : `http://image.tmdb.org/t/p/w185${posterPath}`} />
             </div>
