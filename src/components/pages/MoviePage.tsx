d46b7b35d7f7617ab07aeffddaa352d86742dcc5
Jackie <jackiemay08@gmail.com>
2023-08-11 09:20:37 -0400

    movie page wip

--- src/components/pages/MoviePage.tsx
+++ src/components/pages/MoviePage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './MoviePage.css'
 
 type MoviePageProps = {
@@ -15,17 +16,26 @@ type MoviePageProps = {
 };
 
 export const MoviePage = (props: MoviePageProps) => {
+    const navigate = useNavigate();
+	const goBack = () => {
+		navigate(-1);
+	}
     console.log(props.movieDetails)
     return (
         <div className="moviePage">
+            <div className="container">
             {/* <button onClick={() => props.handleMovieClick(346698)}>Click Me</button> */}
-            <div className="card">
-              {props.movieDetails.posterPath === null ? <img src={'https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg'} alt="card" className="default"/> : <img src={`http://image.tmdb.org/t/p/w185${props.movieDetails.posterPath}`} alt="card"/>}
+                <div className="item">
+                    {props.movieDetails.posterPath === null ? <img src={'https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg'} alt="card" className="default"/> : <img src={`http://image.tmdb.org/t/p/w185${props.movieDetails.posterPath}`} alt="card"/>}
+                </div>
+                <div className="item">
+                    <h3> Title: {props.movieDetails.title}</h3>
+                    <h3> Runtime: {props.movieDetails.runtime} min</h3>
+                    <h3> Year of release: {props.movieDetails.releaseDate.slice( 0 , 4 )}</h3>
+                    <h3> Description: {props.movieDetails.details}</h3>
+                    <button onClick={goBack}>Back</button>	
+                </div>
             </div>
-            <h3> Title: {props.movieDetails.title}</h3>
-            <h3> Runtime: {props.movieDetails.runtime} min</h3>
-            <h3> Year of release: {props.movieDetails.releaseDate.slice( 0 , 4 )}</h3>
-            <h3> Description: {props.movieDetails.details}</h3>
         </div>
     )
 
