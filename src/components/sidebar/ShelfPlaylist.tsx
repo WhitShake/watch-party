e8946a62c7fb5f3669a566a3c89aa8fb2325e532
alyssa-tn <tn.alyssa@gmail.com>
2023-08-13 17:09:11 -0500

    rendered delete playlist button using context menu

--- src/components/sidebar/ShelfPlaylist.tsx
+++ src/components/sidebar/ShelfPlaylist.tsx
@@ -4,9 +4,12 @@ import { Link } from 'react-router-dom';
 import { deleteShelfPlaylist } from '../../firestore_functions/firestore_calls';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../../firebase_setup/firebase';
+import { Menu, Item, useContextMenu} from 'react-contexify';
+import 'react-contexify/dist/ReactContexify.css';
 
 export const ShelfPlaylist = ({title, setPlaylistTitle, setPlaylistPage, setShelf}: ShelfPlaylistProps) => {
     const [user] = useAuthState(auth);
+    const shelfMenuId = `shelf-menu-${title}`;
 
     const populatePlaylistPage = (playlist: string) => {
         setPlaylistTitle(playlist)
@@ -14,16 +17,39 @@ export const ShelfPlaylist = ({title, setPlaylistTitle, setPlaylistPage, setShel
     }
 
     const handleDeletePlaylist = () => {
-        if (user) {
-            deleteShelfPlaylist(user?.uid, title)
-            setShelf(prev => prev.filter(playlist => playlist != title))
+        if (title === 'Watched') {
+            alert("This playlist can't be deleted!")
+        } else {
+            if (user) {
+                deleteShelfPlaylist(user?.uid, title)
+                setShelf(prev => prev.filter(playlist => playlist != title))
+            }
         }
     }
 
+    const { show } = useContextMenu({
+        id: shelfMenuId
+    })
+
+    const displayMenu = (e: React.MouseEvent<HTMLElement>) => {
+        show({
+            event: e
+        })
+    }
+
+
     return (
         <li className="shelf-playlist">
-            <button onClick={handleDeletePlaylist}>Delete {title}</button>
-            <Link onClick={() => populatePlaylistPage(title)} to="/playlist">{title}</Link>
-        </li>
-    )
-}
+            <div onContextMenu={displayMenu} >
+                <Link onClick={() => populatePlaylistPage(title)} to="/playlist">{title}</Link>
+            </div>
+            <Menu id={shelfMenuId}>
+                <Item onClick={handleDeletePlaylist}>
+                Delete Playlist
+                </Item>
+            </Menu>
+            </li>
+    );
+};
+
+
