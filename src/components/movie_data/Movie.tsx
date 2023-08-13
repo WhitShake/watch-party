45dcd3116226da419daf9d2a682d6c53994e4633
alyssa-tn <tn.alyssa@gmail.com>
2023-08-12 19:31:09 -0500

    implemented way to mark movie as watched

--- src/components/movie_data/Movie.tsx
+++ src/components/movie_data/Movie.tsx
@@ -3,13 +3,22 @@ import { MovieProps } from "../prop_types/propsTypes";
 import './Movie.css'
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { addMovieToPlaylist } from "../../firestore_functions/firestore_calls";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase_setup/firebase";
 
 export const Movie = ({posterPath, id}: MovieProps) => {
-
+    const [user] = useAuthState(auth);
     const navigate = useNavigate();
     const handleClick = () => {
         navigate("/movie-details")
     }
+
+    const handleMarkAsWatched = () => {
+        addMovieToPlaylist(user?.uid, 'Watched', {id: id, posterPath: posterPath})
+        console.log("marked as watched")
+    }
+
 
     return (
             <div className="movie-container">
@@ -23,7 +32,7 @@ export const Movie = ({posterPath, id}: MovieProps) => {
                     <div className="dropdown-content">
                         {/* <button onClick={handleClick}>View More Details</button> */}
                         <Link to= {`/movie-details/${id}`}>View More Details</Link>
-                        <a href="#">Mark As Watched</a>
+                        <button onClick={handleMarkAsWatched}>Mark As Watched</button>
                         <a href="#">Add Movie to Playlist</a>
                     </div>
                 </div>
